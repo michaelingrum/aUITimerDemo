bcde55da9c26d2e4ecd128dcc4a46dca360a0d48
michaelingrum <mikeingrum1@gmail.com>
2021-04-12 23:21:57 -0700

    Added comments

--- src/org/csc133/sandbox/SomeSortaForm.java
+++ src/org/csc133/sandbox/SomeSortaForm.java
@@ -8,6 +8,7 @@ public class SomeSortaForm extends Form implements Runnable{
     UITimer time;
     int counter = 0;
     public SomeSortaForm(){
+        //instantiate the timer, you can pass this because the class is Runnable(has a run method)
         time = new UITimer(this);
         unpause();
     }
@@ -17,6 +18,13 @@ public class SomeSortaForm extends Form implements Runnable{
 
     }
     public void unpause(){
+        //Time is milliseconds until timer elapses
+        //The boolean is weather or not the timer should repeat.
+        //We bind a cn1 Form to the timer. I think this is tly if you instantiate UITimer in AppMain.
+        //CN1 has a lot of examples where they make a UITimer, and implement a runnable method using a lambda.
+        //At the end they use .schedule and bind a Form.
+        //I am not sure how to stop the timer if you schedule it like that.
+        //Last note: CN1 makes a note that the timer is not very accurate. 
         time.schedule(1000, true, this);
     }
 
